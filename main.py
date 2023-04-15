c74ec2e345923e8be73405cb77eff431c21bf06c
Anchisatk <117658112+AnchisaNuchmit@users.noreply.github.com>
2023-04-15 15:23:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from functions import buything, discountprice
 
-total = 0
-number = 0
+total = int()
 buying = str("y")
 while buying == "y" :
-    total = buything()
-    totalprice = discountprice()
-    print("Total price :",total)
+    type = int(input("Plase select (1) Espresso (2) Cappuccino (3) Latte (4) Mocha : "))
+    if type >= 5 or type <= 0 :
+        print("Wrong Selection. Plase select on the list only.")
+       
+    
+    else :
+        number = int(input("Plase input number of glasses : "))
+        total = buything(type, number)
+        realtotal = discountprice(total, number)   
+    
+        print("Total price :",realtotal)
     buying = str(input("Do you want to make another order? y/n : "))
 
 print("Thank you :)")
+
+
 
 
 
