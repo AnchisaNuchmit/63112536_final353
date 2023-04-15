a66ccd215fd6c008c80c4fcc6d7fe4993a563e2a
Anchisatk <117658112+AnchisaNuchmit@users.noreply.github.com>
2023-04-15 16:27:34 +0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,30 +1,52 @@
-from main import buything
+from functions import buything, discountprice
 import pytest
 
 @pytest.mark.code # pytest -m code
 def test_input1():
     type = 1
-    excepted_result = "Espresso"
-    actual_result = buything(type)
+    number = 1
+    excepted_result = 55
+    actual_result = buything(type, number)
     assert excepted_result == actual_result
 
 @pytest.mark.code # pytest -m code
 def test_input2():
     type = 2
-    excepted_result = "Cappuccino"
-    actual_result = buything(type)
+    number = 1
+    excepted_result = 60
+    actual_result = buything(type, number)
     assert excepted_result == actual_result
 
 @pytest.mark.code # pytest -m code
 def test_input3():
     type = 3
-    excepted_result = "Latte"
-    actual_result = buything(type)
+    number = 1
+    excepted_result = 65
+    actual_result = buything(type, number)
     assert excepted_result == actual_result
 
 @pytest.mark.code # pytest -m code
 def test_input4():
     type = 4
-    excepted_result = "Mocha"
-    actual_result = buything(type)
+    number = 1
+    excepted_result = 70
+    actual_result = buything(type, number)
+    assert excepted_result == actual_result
+
+@pytest.mark.code # pytest -m code
+def test_true55():
+    total_ = 55
+    number = 1
+    answer = "True"
+    excepted_result = 50
+    actual_result = discountprice(total_, number, answer)
+    assert excepted_result == actual_result
+
+@pytest.mark.code # pytest -m code
+def test_false55():
+    total_ = 55
+    number = 1
+    answer = "False"
+    excepted_result = 55
+    actual_result = discountprice(total_, number, answer)
     assert excepted_result == actual_result
